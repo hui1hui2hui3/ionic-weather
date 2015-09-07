e76fbc565433ea453464864df690a92d63814d41
张少辉 <zhangshaohui@kyee.com.cn>
2015-09-07 10:56:28 +0800

    Init App

---
+++ app/scripts/controllers/homeController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name IonicWeather.controller:MainController
+ * @description
+ * # HomeController
+ */
+angular.module('IonicWeather')
+  .controller('HomeController', function($scope) {
+
+    // do something with $scope
+
+  });
---
+++ app/scripts/controllers/mainController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name IonicWeather.controller:MainController
+ * @description
+ * # MainController
+ */
+angular.module('IonicWeather')
+  .controller('MainController', function($scope) {
+
+    // do something with $scope
+
+  });
